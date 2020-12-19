ee82a168ca023b81e795c3bc572c3191e2f8af18
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-12-19 16:05:16 +0530

    Backend Module styles added

--- admin-dash/src/components/Modules/Module/index.js
+++ admin-dash/src/components/Modules/Module/index.js
@@ -5,40 +5,58 @@ import { getCurrentModule } from '../action';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { SUBMODULE } from '../../../config';
+import './styles.scss';
 
 class Module extends Component {
     componentDidMount() {
         console.log(this.props.match.params.id);
-        this.props.getCurrentModule(this.props.match.params.id)
+        this.props.getCurrentModule(this.props.match.params.id);
     }
     render() {
         const { module } = this.props;
         if (module)
             return (
-                <div>
-                    <div>{module.name}</div>
-                    <div>{SUBMODULE.toUpperCase()}S</div>
-                    <div>
-                        {module.courses && module.courses.map(course =>
-                            <Link to={`/i/${this.props.match.params.type}/course/${course._id}`}>
-                                <div>{course.name}</div>
-                            </Link>
-                        )}
+                <div className="module-container">
+                    <div className="module-navbar">
+                        <div className="module-name">
+                            {module.name.toUpperCase()}
+                        </div>
+                        <span style={{ fontSize: '200%' }}>|</span>
+                        <div className="submodule-name">
+                            {SUBMODULE.toUpperCase()}S
+                        </div>
                     </div>
-                    <Link to={`/i/${this.props.match.params.type}/add-course/${module._id}`}><Button>Add {SUBMODULE}</Button></Link>
+                    <ol className="submodule-list">
+                        {module.courses &&
+                            module.courses.map((course) => (
+                                <Link
+                                    to={`/i/${this.props.match.params.type}/course/${course._id}`}
+                                >
+                                    <li className="submodule-listItem">
+                                        {course.name}
+                                    </li>
+                                </Link>
+                            ))}
+                    </ol>
+                    <Link
+                        to={`/i/${this.props.match.params.type}/add-course/${module._id}`}
+                    >
+                        <Button className="add-button">
+                            + Add {SUBMODULE}
+                        </Button>
+                    </Link>
                 </div>
-            )
-        else
-            return null;
+            );
+        else return null;
     }
 }
 
-const mapStateToProps = state => ({
-    module: state.modulesReducer.currentModule
-})
+const mapStateToProps = (state) => ({
+    module: state.modulesReducer.currentModule,
+});
 
-const mapDispatchToProps = dispatch => ({
-    getCurrentModule: bindActionCreators(getCurrentModule, dispatch)
-})
+const mapDispatchToProps = (dispatch) => ({
+    getCurrentModule: bindActionCreators(getCurrentModule, dispatch),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Module);
