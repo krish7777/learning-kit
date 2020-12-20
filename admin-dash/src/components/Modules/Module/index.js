12ab0cd922d7720c3ab8ab889ba326f00adaf71a
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2020-12-20 18:18:37 +0530

    Changes
    To enable functionality

--- admin-dash/src/components/Modules/Module/index.js
+++ admin-dash/src/components/Modules/Module/index.js
@@ -4,8 +4,12 @@ import { bindActionCreators } from 'redux';
 import { getCurrentModule, updateModule } from '../action';
 import { Link } from 'react-router-dom';
 import { Form, Input, Button } from 'antd';
-import { SUBMODULE } from '../../../config';
+import { baseUrl, SUBMODULE } from '../../../config';
 import './styles.scss';
+import imageCompression from 'browser-image-compression';
+import axios from "axios"
+
+import { Editor } from "@tinymce/tinymce-react";
 
 class Module extends Component {
 
@@ -49,7 +53,7 @@ class Module extends Component {
                 <div className="module-container">
                     <div className="module-navbar">
                         <div className="module-name">
-                            {module.name.toUpperCase()}
+                            {module.name?module.name.toUpperCase():""}
                         </div>
                         <div className="navbar-horizontal">
                             <button
@@ -75,10 +79,12 @@ class Module extends Component {
                     {/* SHOW MODULE DETAILS / SUBMODULES*/}
                     {this.state.selectedWindow === 'module-details' ? (
                         <Form
+                            key={"uniq"+module.introduction}
                             name="update-form"
                             onFinish={this.onFinish}
                             labelCol={{ span: 8 }}
                             wrapperCol={{ span: 16 }}
+                            initialValues={{"name":module.name}}
                         >
                             <Form.Item label="Name">
                                 <Form.Item
@@ -109,10 +115,100 @@ class Module extends Component {
                                         },
                                     ]}
                                 >
-                                    <Input
+                                    {/* <Input
                                         name="introduction"
                                         style={{ width: 260, height: '7vw' }}
                                         placeholder={module.introduction}
+                                    /> */}
+                                    <Editor
+                                        // key={"uniq"+module.introduction}
+                                        apiKey="n7942578kkai07fww91ixztab3vfa874swodd4i2e1ymki4i"
+                                        value={this.state.content}
+                                        init={{
+                                            height: "90vh",
+                                            skin: "oxide-dark",
+                                            content_style: '.mce-content-body{background-color:#041D32; color:white}',
+                                            menubar: false,
+                                            image_dimensions: false,
+                                            image_class_list: [
+                                                { title: 'Responsive', value: 'img-responsive' }
+                                            ],
+                                            formats: {
+                                                nomargin: { selector: 'figure', classes: 'no-margin' }
+                                            },
+                                            plugins: [
+                                                'advlist autolink lists link image',
+                                                'charmap print preview anchor help',
+                                                'searchreplace visualblocks code',
+                                                'insertdatetime media table paste wordcount emoticons',
+                    
+                                            ],
+                                            automatic_uploads: true,
+                                            image_title: true,
+                                            image_caption: true,
+                                            file_picker_types: 'image',
+                                            toolbar:
+                                                'undo redo | formatselect | bold italic | \
+                                alignleft aligncenter alignright | \
+                                bullist numlist outdent indent  | image table  | fontselect fontsizeselect|link | forecolor backcolor  |  emoticons | preview',
+                    
+                    
+                                            file_picker_callback: (callback, value, meta) => {
+                                                console.log("called")
+                                                var input = document.createElement('input');
+                                                input.setAttribute('type', 'file');
+                                                input.setAttribute('accept', 'image/*');
+                                                console.log("this", this)
+                                                input.onchange = async function () {
+                                                    var file = this.files[0];
+                                                    console.log("before compeee")
+                                                    console.log('originalFile instanceof Blob', file instanceof Blob); // true
+                                                    console.log(`originalFile size ${file.size / 1024 / 1024} MB`);
+                                                    const options = {
+                                                        maxSizeMB: 1,
+                                                        maxWidthOrHeight: 920,
+                                                        useWebWorker: true
+                                                    }
+                                                    try {
+                                                        const compressedFile = await imageCompression(file, options);
+                    
+                                                        console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+                                                        console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`);
+                                                        let formData = new FormData();
+                                                        formData.set('expId', '12345')
+                                                        formData.append('file', compressedFile)
+                    
+                                                        await axios.post(`${baseUrl}/api/upload/introduction`, formData).then(
+                                                            res => {
+                                                                console.log("res.data", res.data)
+                                                                console.log("path", process.env.PUBLIC_URL)
+                                                                callback(res.data.location, {
+                                                                    alt: res.data.originalName
+                                                                })
+                                                            }
+                                                        ).catch(err => console.log("error in uploading image"))
+                                                    }
+                                                    catch (err) {
+                                                        console.log("an erron man , probly while compressing")
+                                                    }
+                    
+                                                    // console.log('images', file)
+                                                    // var reader = new FileReader();
+                                                    // reader.onload = function (e) {
+                                                    //   var id = 'blobid' + (new Date()).getTime();
+                                                    //   console.log('name', e.target.result)
+                                                    //   callback('logo192.png', {
+                                                    //     alt: file.name
+                                                    //   })
+                                                    // }
+                                                    // reader.readAsDataURL(file)
+                                                };
+                                                input.click()
+                                            },
+                                            paste_data_images: true,
+                                        }}
+                                        initialValue={module.introduction}
+                                        onEditorChange={this.handleChange}
                                     />
                                 </Form.Item>
                             </Form.Item>
