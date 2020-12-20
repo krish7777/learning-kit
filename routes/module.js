2ff50434c11e85b40791160544dce2d3d247e067
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2020-12-20 19:33:35 +0530

    Module Complete

--- routes/module.js
+++ routes/module.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post('/add', addModule);
 router.get('/get/:module_id', getModule);
-router.post('/update-name/:module_id', updateModule);
+router.post('/update-info/:module_id', updateModule);
 router.get('/all/:type', getAllModules);
 router.get('/course-troubleshoot/:type', getCourseTroubleshoot);
 router.post('/course-troubleshoot/:type', addCourseTroubleshoot);
