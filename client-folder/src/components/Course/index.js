2c5fa335660a19017887411f2d6abc7610f8c1ae
sgr98 <60283755+sgr98@users.noreply.github.com>
2021-01-05 21:16:44 +0530

    Added the exit screen option to the fullScreenButton

--- client-folder/src/components/Course/index.js
+++ client-folder/src/components/Course/index.js
@@ -60,10 +60,6 @@ class Course extends React.Component {
 
   // }, [])
 
-  fullScreenCheck = () => {
-    if (document.fullscreenElement) return;
-    return document.documentElement.requestFullscreen();
-  }
   getOppositeOrientation = () => {
     const { type } = window.screen.orientation;
     return type.startsWith("portrait") ? "landscape" : "portrait";
@@ -79,12 +75,16 @@ class Course extends React.Component {
     await window.screen.orientation.lock(newOrientation);
   }
 
-
-
-
-  openFullscreen = () => {
-    this.fullScreenCheck()
+  fullScreenCheck = () => {
+    if (document.fullscreenElement) {
+      return this.closeFullscreen();
+    }
+    return document.documentElement.requestFullscreen();
   }
+
+  // openFullscreen = () => {
+  //   this.fullScreenCheck()
+  // }
 
   closeFullscreen = () => {
     if (document.exitFullscreen) {
@@ -159,7 +159,7 @@ class Course extends React.Component {
               />
             </svg>
 
-            <div onClick={this.openFullscreen} style={{ cursor: "pointer" }}>
+            <div onClick={this.fullScreenCheck} style={{ cursor: "pointer" }}>
               <svg
                 width="26"
                 height="25"
