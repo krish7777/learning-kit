a7ee00e7795c63dcbf451c254fbb496f3fd02a81
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2021-01-15 19:05:42 +0530

    middleware use cases commented

--- routes/module.js
+++ routes/module.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const isAuth = require('../middlewares/is-auth');
+const isAdmin = require('../middlewares/is-admin');
 const {
     addModule,
     getModule,
@@ -8,14 +9,17 @@ const {
     getCourseTroubleshoot,
     addCourseTroubleshoot,
     updateModule,
+    addTrack
 } = require('../controllers/module');
 const router = express.Router();
 
-router.post('/add', addModule);
+router.post('/add', addModule);//isAdmin
 router.get('/get/:module_id', getModule);
-router.post('/update-info/:module_id', updateModule);
+router.post('/update-info/:module_id', updateModule);//isAdmin
 router.get('/all/:type', getAllModules);
 router.get('/course-troubleshoot/:type', getCourseTroubleshoot);
-router.post('/course-troubleshoot/:type', addCourseTroubleshoot);
+router.post('/course-troubleshoot/:type', addCourseTroubleshoot);//isAdmin
+router.post('/add-track', isAdmin, addTrack)//isAdmin [ONLY FOR IONTERNAL USES]
+
 
 module.exports = router;
