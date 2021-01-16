f991015f7e189265ca261714c96238b9bfb48bb0
AMITKESARI2000 <kesari.amit33@gmail.com>
2021-01-16 20:56:13 +0530

    signup form

--- admin-dash/src/components/Modules/Module/index.js
+++ admin-dash/src/components/Modules/Module/index.js
@@ -28,9 +28,6 @@ class Module extends Component {
     }
 
     changeCurrentWindow = (selectedOptionWord) => {
-        // console.log(
-        //     'NOW SHOWING:' + this.state.selectedWindow
-        // );
         this.setState({
             ...this.state,
             selectedWindow: selectedOptionWord,
@@ -294,7 +291,7 @@ class Module extends Component {
                                 <Button
                                     type="primary"
                                     htmlType="submit"
-                                    style={{ marginLeft: '60%' }}
+                                    style={{ marginLeft: '50%' }}
                                 >
                                     Update
                                 </Button>
