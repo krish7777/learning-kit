33b909e15f12828b92e7087fd7bf91613e7ef326
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-01-24 16:04:16 +0530

    Minimum Working

--- client-folder/src/components/Course/Experiment/DigitalGettingStarted/index.js
+++ client-folder/src/components/Course/Experiment/DigitalGettingStarted/index.js
@@ -1,11 +1,184 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import "./styles.scss";
+import { Link } from 'react-router-dom'
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/scss/image-gallery.scss";
+import "react-image-gallery/styles/css/image-gallery.css";
+import { connect } from "react-redux";
+import { ReactComponent as LeftArrow } from "../../../../assets/images/LeftArrow.svg"
+import { ReactComponent as RightArrow } from "../../../../assets/images/RightArrow.svg"
+import { ReactComponent as SkipIcon } from "../../../../assets/images/SkipIcon.svg"
+import { ReactComponent as TroubleshootIcon } from "../../../../assets/images/TroubleshootIcon.svg"
+import { bindActionCreators } from "redux";
+import { changeCurrentStep, changeStep } from "../../action";
 
-const DigitalGettingStarted = () => {
-    return (
-        <div>
-            DigitalGettingStarted Component
+const SlideShow = (
+  { steps, changeStep, changeCurrentStep, overlayUnread, setOverlayUnread, isGettingStarted, experimentCurrStep, setExperimentStep }
+) => {
+
+
+  const [images, setImages] = useState([]);
+  const [simulations, setSimulations] = useState([]);
+  const [currentStep, setCurrentStep] = useState(0);
+  const inputEl = useRef(null);
+  const [overlayIsOpen, setOverlayIsOpen] = useState(overlayUnread);
+  const [finalOverlayIsOpen, setFinalOverlayIsOpen] = useState(false);
+
+  useEffect(() => {
+    const img = [];
+    const sim = [];
+    for (let i = 0; i < steps.length; i++) {
+        if(steps[i].imagePath){
+            img.push({ original: steps[i].imagePath });
+            sim.push("");
+        }
+        else if(steps[i].simulationLink){
+            img.push("");
+            sim.push(steps[i].simulationLink);
+        }
+        else{
+            img.push("");
+            sim.push("");
+        }
+    }
+    // setStartModalIsOpen(true);
+    // setTimeout(() => {
+    //   setStartModalIsOpen(false);
+    // }, 1500);
+    setImages(img);
+    setSimulations(sim);
+    inputEl.current.slideToIndex(experimentCurrStep)
+  }, [steps]);
+
+  const skipToCode = (index) => {
+    inputEl.current.slideToIndex(index);
+  };
+
+  const closeOverlay = () => {
+    setOverlayIsOpen(false);
+    setOverlayUnread()
+  };
+
+  const goLeft = () => {
+    inputEl.current.slideToIndex(currentStep - 1 === -1 ? 0 : currentStep - 1);
+  };
+
+  const goRight = () => {
+    inputEl.current.slideToIndex(
+      currentStep + 1 === steps.length ? currentStep : currentStep + 1
+    );
+    if (currentStep + 1 === steps.length && !isGettingStarted) {
+      if (finalOverlayIsOpen)
+        changeCurrentStep('ResultsAnalysis')
+      else {
+        setFinalOverlayIsOpen(true)
+      }
+    }
+  };
+
+  const onSlide = (slideNo) => {
+    // console.log("Slide Number", slideNo)
+    setExperimentStep(slideNo)
+    // console.log("Expr current Slide", experimentCurrStep)
+    // console.log("current Step", currentStep)
+    setCurrentStep(slideNo)
+    changeStep(slideNo)
+  }
+
+  const modalChecker = (x) => {
+    onSlide(x)
+  };
+  const dispCheck = () => {
+    return !images[currentStep]?"none":"block";
+  };
+  return (
+    <div className="expshow-slideshow">
+      <div style={{ background: "white" }} className={overlayIsOpen || finalOverlayIsOpen ? "overlayed gallerycontainer" : "gallerycontainer"}>
+            <div className="img_container" style={{display:dispCheck()}}>
+            <ImageGallery
+            ref={inputEl}
+            items={images}
+            infinite={false}
+            showThumbnails={false}
+            showPlayButton={false}
+            showIndex={true}
+            showFullscreenButton={false}
+            showNav={false}
+            
+            onBeforeSlide={modalChecker}
+            />
+            </div>
+        {!images[currentStep]&&
+        <div className="iframe_container" style={{width:"100%",height:"100%"}}>
+            <iframe className="resp-iframe2" src={simulations[currentStep]} allowfullscreen></iframe>
         </div>
-    )
-}
+        }
+        {overlayIsOpen &&
+          <div className="overlay-content">
+            <span>You have successfuly completed the code required to do this experiment. Now upload the code to the Arduino Uno board and lets get started with the experiment.</span>
+            {/* <Link to="/" onClick={(event) => event.preventDefault()}> */}
+            <span onClick={() => changeCurrentStep('Introduction')} style={{ color: "#0C6A9F", fontSize: "medium", cursor: "pointer" }}> HINT: How to upload IDE code to Arduino board</span>
+            {/* </Link> */}
+          </div>
+        }
 
-export default DigitalGettingStarted
+        {finalOverlayIsOpen &&
+          <div className="overlay-content">
+            <span>End of experiment</span>
+          </div>
+        }
+
+      </div>
+
+      <div className={overlayIsOpen || finalOverlayIsOpen ? "overlayed code-step" : "code-step"} style={{ borderBottomLeftRadius: 5, borderBottomRightRadius: 5 }}>
+        Step {currentStep + 1} : {steps[currentStep].description}
+      </div>
+
+      <div className="nav">
+        <div onClick={goLeft} className="left-arrow">
+          <LeftArrow />
+        </div>
+        <div className="divider"></div> {/* Divider Here */}
+        {overlayIsOpen &&
+          <>
+            <div onClick={closeOverlay} className="codeUp-btn">
+              <SkipIcon />
+            CODE UPLOAD SUCCESSFUL
+            </div>
+            <div className="divider"></div> {/* Divider Here */}
+          </>
+        }
+        {!overlayIsOpen && !isGettingStarted &&
+          <div onClick={() => { changeCurrentStep('Troubleshoot') }} className="troubleshoot-btn">
+            <TroubleshootIcon />
+          TROUBLESHOOT
+        </div>
+        }
+        {isGettingStarted &&
+          // <div style={{
+          //   width: "100%"
+          // }}></div>
+          <div className="troubleshoot-btn">
+            CONTACT TEACHER
+        </div>
+        }
+        <div className="divider"></div> {/* Divider Here */}
+        <div onClick={goRight} className="right-arrow">
+          <RightArrow />
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  expSteps: state.courseReducer.currentExpSteps,
+})
+
+const mapDispatchToProps = dispatch => ({
+  changeStep: bindActionCreators(changeStep, dispatch),
+  changeCurrentStep: bindActionCreators(changeCurrentStep, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SlideShow);
